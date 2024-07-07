f7045d0aa95856b4a25f7cfa05d4884b7ea64ade
Ravi Singh <ravi.singh@publicissapient.com>
2024-07-07 21:24:43 +0530

    new TS project babel setup

---
+++ react-webpack/src/form-lib/common/type-helpers.ts
@@ -0,0 +1,6 @@
+export type CreateMutable<Type> = {
+  -readonly [Property in keyof Type]: Type[Property];
+};
+export type CreateReadonly<Type> = {
+  +readonly [Property in keyof Type]: Type[Property];
+};
